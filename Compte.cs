b388228a17aa235cc6d61c77917cb1f812d6658c
nathanjqt <n.jacquet@stag.technofuturtic.education>
2024-12-02 10:56:56 +0100

    ajout fichier

---
+++ Compte.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Compte : ICustomer, IBanker
+    {
+        public void Retrait(double Montant)
+        {
+            Solde -= Montant;
+        }
+        public void Depot(double Montant)
+        {
+            Solde += Montant;
+        }
+    }
+}
